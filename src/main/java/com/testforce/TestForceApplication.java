23c3215c64131a896ef6700750907fc7637dc254
RajasekharReddy45 <36108597+RajasekharReddy45@users.noreply.github.com>
2020-04-13 22:48:22 +0530

    TKT-1

--- src/main/java/com/testforce/TestForceApplication.java
+++ src/main/java/com/testforce/TestForceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestForceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("My Test");
 		SpringApplication.run(TestForceApplication.class, args);
 	}
 
