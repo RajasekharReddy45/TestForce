c8e9434ec5774c8eceb8c3ea2d3204c39876ddfb
RajasekharReddy45 <36108597+RajasekharReddy45@users.noreply.github.com>
2020-04-13 23:03:06 +0530

    TKT-3 change

--- src/main/java/com/testforce/TestForceApplication.java
+++ src/main/java/com/testforce/TestForceApplication.java
@@ -9,7 +9,7 @@ public class TestForceApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(TestForceApplication.class, args);
-		System.out.println("My Test Cherry Pick");
+		System.out.println("My Test Cherry Pick TKT-3");
 	}
 
 }
