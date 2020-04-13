ab77a05c40ad9adf4688520b8eb9043052e37917
US\rrreddy <rrreddy@deloitte.com>
2020-04-13 22:46:16 +0530

    initial commit2

---
+++ src/main/java/com/testforce/TestForceApplication.java
@@ -0,0 +1,13 @@
+package com.testforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testforce/TestForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
